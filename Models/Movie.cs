0651def46345f3d966009e2b80ffc6ea5955f9ab
Jonatan Streith <jonatan@streith.se>
2026-06-15 13:57:38 +0200

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieApi.Models
+{
+    public class Movie
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Title { get; set; }
+        public int Year {  get; set; }
+        public string? Genre { get; set; }
+        public int Duration { get; set; }
+    }
+}
