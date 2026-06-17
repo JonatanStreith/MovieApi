dceff875742feb0e4eb1e237d03395e5ba14425b
Jonatan Streith <jonatan@streith.se>
2026-06-17 16:39:13 +0200

    Creating a movie repository in order to abstract the logic.

---
+++ Repositories/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using MovieApi.Dtos;
+using MovieApi.Models;
+
+namespace MovieApi.Repositories
+{
+    public interface IMovieRepository
+    {
+
+        Task<IEnumerable<Movie>> GetMoviesAsync();
+        Task<Movie?> GetMovieAsync();
+        Task<MovieDetails> GetMovieDetailsAsync();
+        Task AddMovieAsync(MovieCreateDto movie);
+        Task UpdateMovieAsync(MovieUpdateDto movie);
+        Task DeleteMovieAsync(int id);
+    }
+}
---
+++ Repositories/MovieRepository.cs
@@ -0,0 +1,46 @@
+﻿using MovieApi.Dtos;
+using MovieApi.Models;
+
+namespace MovieApi.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+
+        private readonly MovieApiContext _context;
+
+        public MovieRepository(MovieApiContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Task AddMovieAsync(MovieCreateDto movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovieAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie?> GetMovieAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MovieDetails> GetMovieDetailsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> GetMoviesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateMovieAsync(MovieUpdateDto movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
