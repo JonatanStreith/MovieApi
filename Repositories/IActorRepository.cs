b4acc468ae57221652fc028ee8b474eec12bef73
Jonatan Streith <jonatan@streith.se>
2026-06-23 17:34:19 +0200

    Created IActorRepository interface.

---
+++ Repositories/IActorRepository.cs
@@ -0,0 +1,21 @@
+﻿using MovieApi.Dtos;
+using MovieApi.Models;
+
+namespace MovieApi.Repositories
+{
+    public interface IActorRepository
+    {
+        Task<IEnumerable<ActorDto>> GetActorsAsync();
+
+
+        Task<ActorDto> GetActorAsync(int id);
+
+        Task<Actor> AddActorAsync(ActorDto actorDto);
+
+        Task<bool> UpdateActorAsync(int id, ActorDto actor);
+
+        Task<bool> AddActorToMovieAsync(int movieId, int actorId);
+
+        bool ActorExists(int id);
+    }
+}
