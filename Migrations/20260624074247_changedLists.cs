cd05feaa53b5cf7f986aa523d7d8c1f9c870cbda
Jonatan Streith <jonatan@streith.se>
2026-06-24 09:46:27 +0200

    Did a migration due to changed entities.

---
+++ Migrations/20260624074247_changedLists.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedLists : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MovieActors_Actors_ActorId",
+                table: "MovieActors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MovieActors_Movies_MovieId",
+                table: "MovieActors");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MovieActors_Actors_ActorId",
+                table: "MovieActors",
+                column: "ActorId",
+                principalTable: "Actors",
+                principalColumn: "ActorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MovieActors_Movies_MovieId",
+                table: "MovieActors",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "MovieId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MovieActors_Actors_ActorId",
+                table: "MovieActors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MovieActors_Movies_MovieId",
+                table: "MovieActors");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MovieActors_Actors_ActorId",
+                table: "MovieActors",
+                column: "ActorId",
+                principalTable: "Actors",
+                principalColumn: "ActorId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MovieActors_Movies_MovieId",
+                table: "MovieActors",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "MovieId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
