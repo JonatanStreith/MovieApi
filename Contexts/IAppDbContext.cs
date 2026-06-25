42edf4fc818ac8166ce3c23d42c3b25c22389077
Jonatan Streith <jonatan@streith.se>
2026-06-25 15:34:49 +0200

    Created Interface for contexts.

---
+++ Contexts/IAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieApi.Contexts
+{
+    public interface IAppDbContext
+    {
+        public interface IAppDbContext
+        {
+
+            DbSet<MovieApi.Models.Movie> Movies { get; set; }
+            DbSet<MovieApi.Models.MovieDetails> Details { get; set; }
+            DbSet<MovieApi.Models.Review> Reviews { get; set; }
+            DbSet<MovieApi.Models.MovieActor> MovieActors { get; set; }
+            DbSet<MovieApi.Models.Actor> Actors { get; set; }
+
+
+            Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        }
+
+    }
+}
