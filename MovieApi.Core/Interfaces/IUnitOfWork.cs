c77a26b294f77073f0f5b7fe686241f3d45480e1
Jonatan Streith <jonatan@streith.se>
2026-06-30 15:06:43 +0200

    Created UnitOfWork.

---
+++ MovieApi.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using MovieApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApi.Core.Interfaces
+{
+    public interface IUnitOfWork
+    {
+
+        IMovieService Movies { get; }
+        IReviewService Reviews { get; }
+        IActorService Actors { get; }
+        Task CompleteAsync();
+    }
+}
---
+++ MovieApi.Data/Services/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using MovieApi.Core.Interfaces;
+using MovieApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApi.Data.Services
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        public IMovieService Movies => throw new NotImplementedException();
+
+        public IReviewService Reviews => throw new NotImplementedException();
+
+        public IActorService Actors => throw new NotImplementedException();
+
+        public Task CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
