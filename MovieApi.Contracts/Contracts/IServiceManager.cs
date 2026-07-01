a6eaeea1220fb81f73ce1e5d23b8e023eae043f7
Jonatan Streith <jonatan@streith.se>
2026-07-01 16:15:12 +0200

    Created contracts.

---
+++ MovieApi.Contracts/Contracts/IServiceManager.cs
@@ -0,0 +1,15 @@
+﻿using MovieApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApi.Contracts.Contracts
+{
+    internal interface IServiceManager
+    {
+        IMovieService Movies { get; }
+        IReviewService Reviews { get; }
+        IActorService Actors { get; }
+
+    }
+}
