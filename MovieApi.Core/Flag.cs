bc992be2539ba316b654a79c0d1bbbd0aacb4d3a
Jonatan Streith <jonatan@streith.se>
2026-07-07 10:15:05 +0200

    Added a Flag enum to allow passing along conditional data.

---
+++ MovieApi.Core/Flag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApi.Core
+{
+    public enum Flag
+    {
+        Too_Many_Reviews,
+        Movie_Not_Found,
+        Actor_Not_Found,
+        MovieActor_Exists,
+        OK
+    }
+}
